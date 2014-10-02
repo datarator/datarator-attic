b64a897f25568564b38f8617a2c8f60862a4653a
Peter Butkovic <butkovic@gmail.com>
2014-10-03 00:16:02 +0200

    tests for sql template and escapes

--- spec/datarator/out_templates_spec.rb
+++ spec/datarator/out_templates_spec.rb
@@ -6,9 +6,9 @@ module Datarator
 	describe OutTemplates do
 		before(:each) do
 			in_params = InParams.new
+			in_params.document = 'table1'
 			in_params.columns = [ InColumn.new("name1", "foo", "0"), InColumn.new("name2", "foo", "0"), InColumn.new("name3", "foo", "0")]
 			@out_context = OutContext.new in_params
-			@out_context.values = [ 'value1', 'value2', 'value3' ]
 		end
 
 		describe '.pre' do
@@ -35,6 +35,16 @@ module Datarator
 				end
 			end
 
+			context 'with sql template' do
+				before(:each) do
+					@out_context.template = 'sql'
+				end
+
+				it 'returns empty string' do
+					expect(OutTemplates.pre(@out_context)).to eq ''
+				end
+			end
+
 		end
 
 		describe '.item' do
@@ -44,36 +54,87 @@ module Datarator
 				end
 
 				it 'returns comma separated values' do
+					@out_context.values = [ 'value1', 'value2', 'value3' ]
 					expect(OutTemplates.item(@out_context)).to eq "value1,value2,value3\n"
 				end
 
+				it 'returns comma separated values with escaped commas' do
+					@out_context.values = [ '1,1', 'value2', 'value3' ]
+					expect(OutTemplates.item(@out_context)).to eq "'1,1',value2,value3\n"
+				end
 			end
+
+			context 'with sql template' do
+				before(:each) do
+					@out_context.template = 'sql'
+				end
+
+				it 'returns sql inserts with strings escaped' do
+					@out_context.values = [ 'value1', 'value2', 1 ]
+					@out_context.escapes = [ true, true, false ]
+					expect(OutTemplates.item(@out_context)).to eq "INSERT INTO table1 (name1,name2,name3) values ('value1','value2',1);\n"
+				end
+
+				it 'returns sql inserts with escaped character: \'' do
+					@out_context.values = [ 'value1', "foo'bar", 1 ]
+					@out_context.escapes = [ true, true, false ]
+					expect(OutTemplates.item(@out_context)).to eq "INSERT INTO table1 (name1,name2,name3) values ('value1','foo\'\'bar',1);\n"
+				end
+			end
+
 		end
 
 		describe '.post' do
-			context 'with csv template'
-			before(:each) do
-				@out_context.template = 'csv'
+			context 'with csv template' do
+				before(:each) do
+					@out_context.template = 'csv'
+				end
+				it 'returns empty string' do
+					expect(OutTemplates.post(@out_context)).to eq ''
+				end
 			end
-			it 'returns empty string' do
-				expect(OutTemplates.post(@out_context)).to eq ''
+
+			context 'with sql template' do
+				before(:each) do
+					@out_context.template = 'sql'
+				end
+				it 'returns empty string' do
+					expect(OutTemplates.post(@out_context)).to eq ''
+				end
 			end
+
 		end
 
 		describe '.empty' do
-			context 'with csv template'
-			before(:each) do
-				@out_context.template = 'csv'
+			context 'with csv template' do
+				before(:each) do
+					@out_context.template = 'csv'
+				end
+				it 'returns empty string' do
+					expect(OutTemplates.empty(@out_context)).to eq ''
+				end
 			end
-			it 'returns empty string' do
-				expect(OutTemplates.empty(@out_context)).to eq ''
+
+			context 'with sql template' do
+				before(:each) do
+					@out_context.template = 'sql'
+				end
+				it 'returns string: NULL' do
+					expect(OutTemplates.empty(@out_context)).to eq 'NULL'
+				end
 			end
 		end
 
 		describe '.validate' do
-			context 'for existing output template' do
+			context 'for csv output template' do
 				it 'passes validation' do
 					expect(OutTemplates.validate 'csv').to eq nil
+				end
+			end
+
+			context 'for sql output template' do
+				it 'passes validation' do
+					expect(OutTemplates.validate 'sql').to eq nil
 				end
 			end
 
